f002a686dfd9d06282fda8a82b4a936f41e31237
9ssi7 <contact@9ssi7.dev>
2024-07-18 06:03:05 +0300

    feat: remove cloudflare adapter

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: "server",
-  adapter: cloudflare()
 });
