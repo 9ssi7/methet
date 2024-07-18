8f49ea8b52d4bad4faf49aa6c9069af046bcced2
9ssi7 <contact@9ssi7.dev>
2024-07-18 06:15:59 +0300

    feat: add node adapter

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,9 +1,15 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
